dd50ff4ba6178e803019b78a303f8885eb774db4
Mclarenlife <mclarenlife@qq.com>
2025-07-21 03:10:13 +0800

    Initial Commit

---
+++ 7DYASTests/_DYASTests.swift
@@ -0,0 +1,17 @@
+//
+//  _DYASTests.swift
+//  7DYASTests
+//
+//  Created by Mclarenlife on 2025/7/21.
+//
+
+import Testing
+@testable import _DYAS
+
+struct _DYASTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
